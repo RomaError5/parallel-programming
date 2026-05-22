3e0a14322976a1a80510cecf375146ea2c28bcb8
RomaError5 <RomaError5@gmail.com>
2026-05-22 14:43:59 +0400

    Update .bat file

--- start-matrixmultiply.bat
+++ start-matrixmultiply.bat
@@ -2,8 +2,8 @@
 setlocal enabledelayedexpansion
 
 rem Использование: start-matrixmultiply.bat [число_процессов] [размер_матрицы]
-set NUM_PROCS=%1
-if "%NUM_PROCS%"=="" set NUM_PROCS=1
+set BLOCK_SIZE=%1
+if "%BLOCK_SIZE%"=="" set BLOCK_SIZE=8
 set MATRIX_SIZE=%2
 if "%MATRIX_SIZE%"=="" set MATRIX_SIZE=200
 
@@ -26,7 +26,7 @@ python matrix_generate.py %MATRIX_SIZE%
 
 echo.
 echo Перемножаем матрицы...
-bin\x64\Debug\mpiexec.exe -np %NUM_PROCS% bin\x64\Debug\MatrixMultiply.exe A.txt B.txt result.txt
+bin\x64\Debug\MatrixMultiply.exe A.txt B.txt result.txt %BLOCK_SIZE%
 
 echo.
 echo Проверяем результат...
