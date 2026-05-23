c6ae2678179d22d8fb5a46aadb54ae9cf201e5cc
RomaError5 <RomaError5@gmail.com>
2026-05-23 14:06:46 +0400

    Delete files (compilation on a supercomputer)

--- build-matrixmultiply.bat
+++
@@ -1,8 +0,0 @@
-call "%ProgramFiles%\Microsoft Visual Studio\18\Community\Common7\Tools\VsDevCmd.bat"
-
-msbuild ParallelProgramming.slnx ^
-    -p:Configuration=Debug ^
-    -p:Platform=x64 ^
-    -maxCpuCount ^
-    -nologo ^
-    -v:minimal
